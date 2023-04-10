4790140718b861f80e3231e76bca15cc102da49a
Lucas <teodorolucasc@gmail.com>
2023-04-10 03:56:07 -0300

    Formulario Base Html

--- tca/views.py
+++ tca/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def tca(request):
+    if request.method == "GET":
+        return render(request, 'tca.html')
+    elif request.method == "POST":
+        tcaNum = request.POST.get("numTCA")
+        razaoSocial = request.POST.get("razaoSocial")
+        endereco = request.POST.get("endereco")
+        email = request.POST.get("email")
+        cpfcnpj = request.POST.get("cpfcnpj")
 
-    return render(request, 'tca.html')
+        print(razaoSocial)
+        print(endereco)
+        print(cpfcnpj)
+        return HttpResponse('Teste')
