287f96506bb187d2c04ee7283727080424384a03
Lucas <teodorolucasc@gmail.com>
2023-04-10 04:44:17 -0300

    Cadastro no Banco de Dados

--- tca/models.py
+++ tca/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class TCA(models.Model):
+
+    tcaNum = models.CharField(max_length = 10)
+    razaoSocial = models.CharField(max_length = 100)
+    endereco = models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 40)
+    cpfcnpj = models.CharField(max_length = 15)
+
+    def __str__ (self) -> str:
+        return self.tcaNum
+
+class AC(models.Model):
+
+    ACNum = models.CharField(max_length = 10)
+    area = models.CharField(max_length = 10)
+    
+    def __str__ (self) -> str:
+        return self.ACNum
--- tca/views.py
+++ tca/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TCA, AC
 
 def tca(request):
     if request.method == "GET":
         return render(request, 'tca.html')
+    
     elif request.method == "POST":
-        tcaNum = request.POST.get("numTCA")
+
+        tcaNum = request.POST.get("tcaNUM")
         razaoSocial = request.POST.get("razaoSocial")
         endereco = request.POST.get("endereco")
         email = request.POST.get("email")
         cpfcnpj = request.POST.get("cpfcnpj")
 
-        print(razaoSocial)
-        print(endereco)
-        print(cpfcnpj)
-        return HttpResponse('Teste')
+        newTCA = TCA.objects.filter(cpfcnpj=cpfcnpj)
+
+        if newTCA.exists(): #retorno se cpfcnpj ja existe
+            return render(request, 'tca.html', {'tcaNum':tcaNum, 'razaoSocial':razaoSocial, 'endereco':endereco, 'email':email })
+
+        newTCA = TCA(
+            tcaNum = tcaNum,
+            razaoSocial = razaoSocial,
+            endereco = endereco,
+            email = email,
+            cpfcnpj = cpfcnpj
+        )
+
+        newTCA.save()
+
+        return render(request, 'tca.html')
