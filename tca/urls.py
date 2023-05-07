d5bb2fcb2c146e0d2d9e51f1718314e4629684cb
Lucas <teodorolucasc@gmail.com>
2023-05-07 04:28:25 -0300

    ATT

--- tca/urls.py
+++ tca/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tca, name='tca'),
+    path('',views.menutca, name='menutca'),
+    path('cadastrartca/', views.cadastrartca, name='cadastrartca'),
+    path('listartca/',views.listartca, name='listartca'),
 ]
